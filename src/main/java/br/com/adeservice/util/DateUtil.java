227458d73de4eab40fedae18c3473c517985b4d1
fabiomasson <fabiomasson@gmail.com>
2011-12-27 18:10:15 -0200

    added utility class to manage dates

---
+++ src/main/java/br/com/adeservice/util/DateUtil.java
@@ -0,0 +1,17 @@
+package br.com.adeservice.util;
+
+import java.util.Date;
+
+/**
+ * Utility class to manage dates
+ * Dec 27, 2011
+ * @author fabio
+ *
+ */
+public class DateUtil {
+	
+	public static Date now(){
+		return new Date();
+	}
+
+}
