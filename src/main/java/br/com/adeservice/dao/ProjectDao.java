e9ed77dd6ae1c56d67080fe976dabf95a6f95a52
fabiomasson <fabiomasson@gmail.com>
2012-04-10 22:18:36 -0300

    added project dao

---
+++ src/main/java/br/com/adeservice/dao/ProjectDao.java
@@ -0,0 +1,21 @@
+package br.com.adeservice.dao;
+
+import com.adesoft.beans.AdeApi;
+
+import br.com.adeservice.model.Project;
+
+/**
+ * Apr 2, 2012
+ * @author fabio
+ *
+ */
+public class ProjectDao extends AbstractBaseDao<Project>{
+
+	public ProjectDao(final AdeApi api) {
+		super(api);
+		// TODO Auto-generated constructor stub
+	}
+
+
+
+}
